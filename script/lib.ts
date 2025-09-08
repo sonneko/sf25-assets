48d7b2e3230747ecff3d6d995a20199028b7b1c0
Sonneko <152277125+sonneko@users.noreply.github.com>
2025-09-08 15:57:36 +0900

    Update lib.ts

--- script/lib.ts
+++ script/lib.ts
@@ -55,7 +55,7 @@ export function parseAsYaml(content: string): unknown {
 export function parseAsMd(content: string): string {
     // try {
     const window = new JSDOM("").window;
-    const DONPurify = createDomPurify(window)
+    const DOMPurify = createDOMPurify(window)
 
         const options: MarkedOptions = {};
         const dangerousResultHtml = marked(content, options);
